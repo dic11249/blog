f944673f9f7d60db635fa54a23057bb30103c5dc
dic11249 <dic11249@gmail.com>
2019-06-15 23:27:21 +0800

    add routing
    index, about, contact, posts, posts/{id}

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,27 @@
 |
 */
 
+// 首頁
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+// 關於我們
+Route::get('/about', function () {
+    return view('about');
+});
+
+// 聯絡我們
+Route::get('/contact', function () {
+    return view('contace');
+});
+
+// 文章列表首頁  view(資料夾)->posts(資料夾)->list(檔案)
+Route::get('/posts', function () {
+    return view('posts.list');
+});
+
+// 指定文章並帶入文章id  /posts/9487
+Route::get('/posts/{id}', function ($id) {
+    return view('posts.show');
 });
