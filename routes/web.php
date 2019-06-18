227847909617a91b50405ee5db047e6cdf2ad825
dic11249 <dic11249@gmail.com>
2019-06-18 23:05:06 +0800

    Revert "add fake posts data/ posts loop @post list"
    This reverts commit e81d2d18a97cda3a4457988fb62fa7594e5b0d80.

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,7 @@ Route::get('/contact', function () {
 
 // 文章列表首頁  view(資料夾)->posts(資料夾)->list(檔案)
 Route::get('/posts', function () {
-    $posts = [1,2,3,4,5];
-    return view('posts.list', ['posts'=>$posts]);
+    return view('posts.list');
 });
 
 // 指定文章並帶入文章id  /posts/9487
