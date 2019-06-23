c2bba3dc7efa2de5e5bd27b6e3f0b15ac98f6281
dic11249 <dic11249@gmail.com>
2019-06-23 22:42:52 +0800

    add authorize

--- app/Http/Requests/StoreBlogPost.php
+++ app/Http/Requests/StoreBlogPost.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class StoreBlogPost extends FormRequest
 {
@@ -13,7 +14,11 @@ class StoreBlogPost extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        $post = request()->post;
+        if($post->user_id === Auth::id())
+            return true;
+        else
+            return false;
     }
 
     /**
