63e0bc672fe5c127367449c7be325f331048e6fa
dic11249 <dic11249@gmail.com>
2019-07-03 23:13:22 +0800

    add update post thumbnail

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -134,6 +134,14 @@ class PostController extends Controller
     public function update(StoreBlogPost $request, Post $post)
     {
         $post->fill($request->all());
+
+        if(is_null(!$request->file('thumbnail'))){
+            $path = $request->file('thumbnail')->store('public');
+            $path = str_replace('public/', '/storage/', $path);
+
+            $post->thumbnail = $path;
+        }
+
         $post->save();
 
         //remove old relationships
--- resources/views/posts/_form.blade.php
+++ resources/views/posts/_form.blade.php
@@ -24,7 +24,10 @@
          <input type="text" class="form-control" name="title" value="{{ $post->title }}">
      </div>
      <div class="form-group">
-         <label>Thumbnail</label>
+         <label class="d-block">Thumbnail</label>
+         @if ($post->thumbnail)
+            <img width="320" src="{{ $post->thumbnail }}" alt="thumbnail">
+         @endif
          <div class="custom-file">
              <input type="file" class="custom-file-input" name="thumbnail">
              <label class="custom-file-label" for="customFile">Choose file</label>
