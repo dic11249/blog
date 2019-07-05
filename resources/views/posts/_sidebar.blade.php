515c9e701ea10be375a72d6d60daaaeb0b25bbf4
dic11249 <dic11249@gmail.com>
2019-07-05 22:59:35 +0800

    update sidebar

--- resources/views/posts/_sidebar.blade.php
+++ resources/views/posts/_sidebar.blade.php
@@ -1,9 +1,13 @@
 @php
 use App\Category;
 use App\Tag;
+use App\Post;
+use App\Comment;
 
 $categories = Category::all();
 $tags = Tag::has('posts')->withCount('posts')->orderBy('posts_count', 'desc')->get();
+$latestPosts = Post::orderBy('created_at','desc')->take(3)->get();
+$latestComments = Comment::orderBy('created_at','desc')->take(3)->get();
 @endphp
 <!--search widget-->
 <div class="widget">
@@ -24,39 +28,23 @@ $tags = Tag::has('posts')->withCount('posts')->orderBy('posts_count', 'desc')->g
         <h6 class="text-uppercase">latest post</h6>
     </div>
     <ul class="widget-latest-post">
-        <li>
-            <div class="thumb">
-                <a href="#">
-                    <img src="/assets/img/post/post-thumb.jpg" alt="" />
-                </a>
-            </div>
-            <div class="w-desk">
-                <a href="#">Old Father Getup</a>
-                April 25, 2014
-            </div>
-        </li>
-        <li>
-            <div class="thumb">
-                <a href="#">
-                    <img src="/assets/img/post/post-thumb-2.jpg" alt="" />
-                </a>
-            </div>
-            <div class="w-desk">
-                <a href="#">Represent is the best way</a>
-                March 28, 2014
-            </div>
-        </li>
-        <li>
-            <div class="thumb">
-                <a href="#">
-                    <img src="/assets/img/post/post-thumb-3.jpg" alt="" />
-                </a>
-            </div>
-            <div class="w-desk">
-                <a href="#">Alone with the music</a>
-                May 05, 2014
-            </div>
-        </li>
+        @foreach ($latestPosts as $key => $post)
+            <li>
+                <div class="thumb">
+                    <a href="/posts/{{ $post->id }}">
+                        @if ($post->thumbnail)
+                            <img src="{{ $post->thumbnail }}" alt="thumbnail" />
+                        @else
+                            <img src="/assets/img/post/post-thumb.jpg" alt="" />
+                        @endif
+                    </a>
+                </div>
+                <div class="w-desk">
+                    <a href="#">{{ $post->title }}</a>
+                    {{ $post->created_at->format('F d, Y') }}
+                </div>
+            </li>
+        @endforeach
     </ul>
 </div>
 <!--latest post widget-->
@@ -81,14 +69,10 @@ $tags = Tag::has('posts')->withCount('posts')->orderBy('posts_count', 'desc')->g
         <h6 class="text-uppercase">Latest comments </h6>
     </div>
     <ul class="widget-comments">
-        <li>Jonathan on <a href="javascript:;">Vesti blulum quis dolor </a>
-        </li>
-        <li>Jane Doe on <a href="javascript:;">Nam sed arcu tellus</a>
-        </li>
-        <li>Margarita on <a href="javascript:;">Fringilla ut vel ipsum </a>
-        </li>
-        <li>Smith on <a href="javascript:;">Vesti blulum quis dolor sit</a>
-        </li>
+        @foreach ($latestComments as $key => $comment)
+            <li>{{ $comment->name }} on <a href="/posts/{{ $comment->post->id }}">{{ $comment->post->title}} </a>
+            </li>
+        @endforeach
     </ul>
 </div>
 <!--comments widget-->
