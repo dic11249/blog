c76e0c45ec22b8eff3fcac8e7dda74847a353436
dic11249 <dic11249@gmail.com>
2019-07-05 23:14:45 +0800

    add pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -74,26 +74,10 @@
                     <!--classic image post-->
                 @endforeach
 
-                <!--pagination-->
+
                 <div class="text-center">
-                    <ul class="pagination custom-pagination">
-                        <li><a href="#">Prev</a>
-                        </li>
-                        <li class="active"><a href="#">1</a>
-                        </li>
-                        <li><a href="#">2</a>
-                        </li>
-                        <li><a href="#">3</a>
-                        </li>
-                        <li><a href="#">4</a>
-                        </li>
-                        <li><a href="#">5</a>
-                        </li>
-                        <li><a href="#">Next</a>
-                        </li>
-                    </ul>
+                    {{ $posts->links() }}
                 </div>
-                <!--pagination-->
 
             </div>
             <div class="col-md-4">
